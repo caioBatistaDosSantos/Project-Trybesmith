55e7e0c56dba23f0b7b63503a6b4a394829f60a6
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-06-09 16:16:22 -0300

    feat: added productModel.ts in models

---
+++ src/models/productModel.ts
@@ -0,0 +1,27 @@
+import { ResultSetHeader } from 'mysql2';
+import connection from './connection';
+// import IterfaceProduct from '../interfaces/productInterface';
+
+const getProductsAll = async () => {
+  const query = 'SELECT * FROM Trybesmith.Products ORDER BY id';
+
+  const [products] = await connection.execute(query);
+
+  return products;
+};
+
+const createProduct = async (name: string, quantity: string) => {
+  const query = 'INSERT INTO products (name, quantity) VALUES (?, ?)';
+
+  const [registeredProduct] = await connection.execute<ResultSetHeader>(query, [name, quantity]);
+
+  const product = {
+    id: registeredProduct.insertId,
+    name,
+    quantity,
+  };
+
+  return product;
+};
+ 
+export { getProductsAll, createProduct };
